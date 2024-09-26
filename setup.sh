5222d01f7915828e2af81a471a57b822cc17cb6c
UttejAttili <94598515+UttejAttili@users.noreply.github.com>
2024-09-25 20:20:18 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+
+sudo apt-get install git
+
+sudo apt-get install python3-pip
+
+git clone your_repository_url
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install -r requirements.txt
+
+python chatbot-app.py
